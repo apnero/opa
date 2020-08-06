5886b3b6ce71bda36ca0f37ae0c75885e8dbac93
CommitUser <CommitUser@nerodm.com>
2020-08-05 23:30:50 -0400

    one

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/opa/" : "/",
-};
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/opa/" : "/",
+// };
