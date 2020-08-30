7876089de16e06913f4989ebc0c2c38e795cf7c8
CommitUser <CommitUser@nerodm.com>
2020-08-30 11:17:38 -0400

    images, build

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -1,28 +1,33 @@
 <template>
   <v-container fluid class="pa-0">
     <v-row no-gutters>
-      <v-col cols="3" v-for="num in 25" :key="num">
-        <v-card tile :color="colors[Math.floor(Math.random()*133)].hex"  > 
-              <v-responsive :aspect-ratio="1/1" min-height="20">
-        <v-card-text>
-          This card will always be 16:9 (unless you put more stuff in it)
-        </v-card-text>
-      </v-responsive>
-        </v-card>
+      <v-col lg="3" md="4" sm="6" cols="6" v-for="num in 25" :key="num">
+        <v-hover v-slot:default="{ hover }">
+          <v-card
+            tile
+            :color="colors[Math.floor(Math.random() * 133)].hex"
+            :elevation="hover ? 12 : 2"
+          >
+            <v-responsive :aspect-ratio="1 / 1">
+              <v-card-text>
+                OPA
+              </v-card-text>
+            </v-responsive>
+          </v-card>
+        </v-hover>
       </v-col>
     </v-row>
-    {{colors.length}}
   </v-container>
 </template>
 
 <script>
-import colors from "@/assets/crayola.json"
+import colors from "@/assets/crayola.json";
 
 export default {
   data() {
     return {
-      colors
-    }
-  }
+      colors,
+    };
+  },
 };
 </script>
