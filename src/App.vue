383dc96131c442de98a31e4d7b192c5fcd21e38c
CommitUser <CommitUser@nerodm.com>
2020-09-07 22:05:28 -0400

    toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <template>
   <v-app>
+     <v-app-bar fixed
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      prominent
+      dark
+    >
+      <v-toolbar-title>OPA Marketing</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-facebook</v-icon>
+      </v-btn>
+      <v-btn href="https://instagram.com/nai_brito_ct" icon>
+        <v-icon>mdi-instagram</v-icon>
+      </v-btn>
+      <v-btn icon
+        href="sms:+14752189321"
+      >
+        <v-icon>mdi-comment-text</v-icon>
+      </v-btn>
+      <v-btn href="mailto:opamarketingdanbury@gmail.com" icon>
+        <v-icon>mdi-email</v-icon>
+      </v-btn>
+    </v-app-bar>
     <v-main >
       <MainScreen/>
     </v-main>
--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -1,27 +1,5 @@
 <template>
   <v-container fluid class="pa-0">
-    <v-toolbar
-      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-      prominent
-      dark
-    >
-      <v-toolbar-title>OPA Marketing</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-facebook</v-icon>
-      </v-btn>
-      <v-btn href="https://instagram.com/nai_brito_ct" icon>
-        <v-icon>mdi-instagram</v-icon>
-      </v-btn>
-      <v-btn icon
-        href="sms:+14752189321&body=Hi%2520there%252C%2520I%2527d%2520like%2520to%2520place%2520an%2520order%2520for..."
-      >
-        <v-icon>mdi-comment-text</v-icon>
-      </v-btn>
-      <v-btn href="mailto:'opamarketingdanbury@gmail.com'" icon>
-        <v-icon>mdi-email</v-icon>
-      </v-btn>
-    </v-toolbar>
     <Panels :language="language" />
     <v-bottom-navigation dark style="z-index:20" height="32px" grow app>
       <v-btn @click="language = 0">
